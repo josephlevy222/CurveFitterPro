5c4b46f9c1de2dbf7112a1eaf4b15b782ed8eb5d
josephlevy222 <josephlevy222@gmail.com>
2026-02-19 09:51:56 -0800

    Initial Commit

---
+++ CurveFitterPro/CurveFitterProApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurveFitterProApp.swift
+//  CurveFitterPro
+//
+//  Created by Joseph Levy on 2/19/26.
+//
+
+import SwiftUI
+
+@main
+struct CurveFitterProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
